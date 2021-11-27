260105a8ae34a4abddab6fc223616d758cb70abc
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-27 18:28:45 +0300

    add empty cell and table view on moviesVC

--- Movies/Controllers/MoviesListViewController.swift
+++ Movies/Controllers/MoviesListViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class MoviesListViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
+        setupViews()
     }
 }
+
+//MARK: - Private Methods
+private extension MoviesListViewController {
+    func setupViews() {
+        tableView.separatorStyle = .none
+        tableView.register(MovieTableViewCell.nib, forCellReuseIdentifier: MovieTableViewCell.identifier)
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension MoviesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+//MARK: - UITableViewDelegate
+extension MoviesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected item by index \(indexPath.row)")
+    }
+}
+
